003eb71686c91037d5921f6157063509e81d3971
Tom Ellis <tom-git@jaguarpaw.co.uk>
2026-07-11 11:11:10 +0100

    (refactor) Introduce AnalyticsPixels abstraction layer

--- src/Distribution/Server/Features/AnalyticsPixels/Acid.hs
+++ src/Distribution/Server/Features/AnalyticsPixels/Acid.hs
@@ -1,10 +1,23 @@
 module Distribution.Server.Features.AnalyticsPixels.Acid
-  ( analyticsPixelsStateComponent
+  ( acidStore
   ) where
 
 import qualified Distribution.Server.Features.AnalyticsPixels.State as Acid
+import Distribution.Server.Features.AnalyticsPixels.Store
 import Distribution.Server.Framework
 import Distribution.Server.Framework.BackupRestore
+
+acidStore :: FilePath -> IO Backend
+acidStore stateDir = do
+  analyticsPixelsState <- analyticsPixelsStateComponent stateDir
+  return Backend {
+      backendStore = Store {
+        getPackageAnalyticsPixels = \name -> queryState analyticsPixelsState (Acid.AnalyticsPixelsForPackage name)
+      , addPackageAnalyticsPixel = \name pixel -> updateState analyticsPixelsState (Acid.AddPackageAnalyticsPixel name pixel)
+      , removePackageAnalyticsPixel = \name pixel -> updateState analyticsPixelsState (Acid.RemovePackageAnalyticsPixel name pixel)
+      }
+    , backendState = [abstractAcidStateComponent analyticsPixelsState]
+    }
 
 -- | Define the backing store (i.e. database component)
 analyticsPixelsStateComponent :: FilePath -> IO (StateComponent AcidState Acid.AnalyticsPixelsState)
